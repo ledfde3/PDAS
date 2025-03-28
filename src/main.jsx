c33594bbd1a1aa70c7fede4626e015088acf7c4d
ledfde3 <ayledmafer2807@gmail.com>
2025-03-27 18:55:28 -0700

    agregado main

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,10 @@
-function ProfileCard({ title, handle, image}) {
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-  return(
-    <div className="card">
-      <div className="card-image">
-        <figure className="image is-1by1">
-          <img src={image} alt="" vidth={200}height={200} />
-        </figure>
-    </div>
-
-    <div className="card-content">
-      <div className="media-content">
-        <p className="title is-4"> Title is {title}</p>
-        <p className="subtitle is-6"> Handle is {handle} </p>
-        </div>
-      </div>
-    </div>
-  )
-}
-
-export default ProfileCard
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
