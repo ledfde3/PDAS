6b1105a4815b2fec30d4ea870158729082f007b8
ledfde3 <ayledmafer2807@gmail.com>
2025-03-27 19:42:45 -0700

    agregado App

--- src/App.jsx
+++ src/App.jsx
@@ -17,16 +17,24 @@ function App() {
                 <ProfileCard 
                 title="Alexa"
                 handle="@alexa24"
-                image ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQySxmPfYLTLVKi_Q-Lqhcx2sqbPqYefYIX3DxYXG1wA1Z8cwk9L7BpSlvuHQ4YK_geISA&usqp=CAU"
+                image ="https://media.es.wired.com/photos/67bf79bd6a1885df467799ad/1:1/w_900,h_900,c_limit/alexa-logo-1600x900-1.jpg"
                 />
                 
               </div>
               <div className="column is-4">
-                <ProfileCard />
+                <ProfileCard 
+                title="HarryPotter"
+                handle="@HarryPotter001"
+                image ="https://static.vecteezy.com/system/resources/thumbnails/022/230/278/small/harry-potter-hogwarts-logo-in-cartoon-doodle-style-illustration-isolated-on-white-background-free-vector.jpg"
+                />
                 
               </div>
               <div className="column is-4">
-                <ProfileCard />
+                <ProfileCard 
+                title="Nirvana"
+                handle="@Nirvana002"
+                image ="https://cdn-images.dzcdn.net/images/cover/7b4b3ccd52e58bec9dc0c8ea1973b177/0x1900-000000-80-0-0.jpg"
+                />
                 
               </div>
             </div>
